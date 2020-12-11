a4431a45b27cbfbe72be201c9c8868b3ab0318dd
ceylinbrooks <ceylinbrooks@gmail.com>
2020-12-11 13:33:10 -0500

    Lab Update

--- prework/app.js
+++ prework/app.js
@@ -3,7 +3,7 @@
 var global = 'I am a global varible';
 
 function scopeMaker(){
-  var local = 'I am a local variable';
+  let local = 'I am a local variable';
   console.log(global, 'logged from inside scopeMaker().');
   console.log(local, 'logged from inside scopeMaker().');
 }
